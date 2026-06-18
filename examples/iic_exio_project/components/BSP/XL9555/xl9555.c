5a5de3a6dcb94e6c9f2854f9dc33e1a73f6a52d4
zx <bkssxzdwy@126.com>
2026-06-18 10:11:25 +0800

    从零开始手搓 AI 桌面机器人：从寄存器配置到命令序列，手把手带你打通ESP32-S3的I2C开发全流程

---
+++ examples/iic_exio_project/components/BSP/XL9555/xl9555.c
@@ -0,0 +1,5 @@
+#include "xl9555.h"
+
+esp_err_t xl9555_init(void){
+    return ESP_OK;
+}
---
+++ examples/iic_exio_project/main/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "nvs_flash.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "pwm.h"
+
+const char *TAG_MAIN = "PWM";
+
+void app_main(void)
+{
+    esp_err_t ret;
+
+    ret = nvs_flash_init(); /* 初始化NVS */
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    ESP_ERROR_CHECK(ret);
+
+    pwm_init();
+
+    while (1)
+    {
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
